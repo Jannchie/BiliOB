ae61caec4c3d7c711f9eef164db74ae76da7f31e
jannchie <jannchie@gmail.com>
2018-10-13 13:50:10 +0800

    online追加追踪分区信息

--- biliob_spider/spiders/online.py
+++ biliob_spider/spiders/online.py
@@ -26,15 +26,42 @@ class OnlineSpider(scrapy.spiders.Spider):
             title_list = video_list.xpath('./a/p/text()').extract()
             watch_list = video_list.xpath('./p/b/text()').extract()
             author_list = video_list.xpath('./div[1]/a/text()').extract()
+            href_list = video_list.xpath('./a/@href').extract()
             for i in range(len(title_list)):
                 item = VideoOnline()
                 item['title'] = title_list[i]
                 item['author'] = author_list[i]
                 item['data'] = {'datetime':datetime.datetime.now(),'number':watch_list[i]}
-                yield item
-
+                item['aid'] = href_list[i][9:-1]
+                # 为了爬取分区等数据，需要进入每一个视频的详情页面进行抓取
+                yield Request(
+                    "https://www.bilibili.com" + href_list[i],
+                    meta={'item': item},
+                    callback=self.detailParse)
         except Exception as error:
             # 出现错误时打印错误日志
             logging.error("视频爬虫在解析时发生错误")
+            logging.error(response.url)
+            logging.error(error)
+
+    def detailParse(self,response):
+        try:
+            item = response.meta['item']
+            c = response.xpath("//span[@class='crumb'][2]/a/text()").extract()
+            if c != []:
+                item['channel'] = response.xpath("//span[@class='crumb'][2]/a/text()").extract()[0]
+            else:
+                item['channel'] = '番剧'
+
+            c = response.xpath("//span[@class='crumb'][3]/a/text()").extract()
+            if c != []:
+                item['subChannel'] = response.xpath("//span[@class='crumb'][3]/a/text()").extract()[0]
+            else:
+                item['subChannel'] = '番剧'    
+
+            yield item
+        except Exception as error:
+            # 出现错误时打印错误日志
+            logging.error("视频爬虫在解析细节时发生错误")
             logging.error(response.url)
             logging.error(error)
