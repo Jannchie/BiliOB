e7b5374df7fcfa36ca7ba2d4755ef3da1e35dcbc
jannchie <jannchie@gmail.com>
2018-11-01 19:57:55 +0800

    添加tag爬取，添加全站所有排行的爬取

--- biliob_spider/spiders/author_auto_add.py
+++ biliob_spider/spiders/author_auto_add.py
@@ -12,7 +12,21 @@ import datetime
 class AuthorAutoAddSpider(scrapy.spiders.Spider):
     name = "authorAutoAdd"
     allowed_domains = ["bilibili.com"]
-    start_urls = ['https://www.bilibili.com/ranking']
+    start_urls = [
+        'https://www.bilibili.com/ranking',
+        'https://www.bilibili.com/ranking/all/1/0/3',
+        'https://www.bilibili.com/ranking/all/168/0/3',
+        'https://www.bilibili.com/ranking/all/3/0/3',
+        'https://www.bilibili.com/ranking/all/129/0/3',
+        'https://www.bilibili.com/ranking/all/4/0/3',
+        'https://www.bilibili.com/ranking/all/36/0/3',
+        'https://www.bilibili.com/ranking/all/160/0/3',
+        'https://www.bilibili.com/ranking/all/119/0/3',
+        'https://www.bilibili.com/ranking/all/155/0/3',
+        'https://www.bilibili.com/ranking/all/5/0/3',
+        'https://www.bilibili.com/ranking/all/181/0/3'
+    ]
+
     custom_settings = {
         'ITEM_PIPELINES': {
             'biliob_spider.pipelines.AuthorPipeline': 300
