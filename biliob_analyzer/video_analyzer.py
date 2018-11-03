29d8765eea463a10d08a9aea1d8818f5321701f2
jannchie <jannchie@gmail.com>
2018-11-03 19:03:01 +0800

    修复分析bug

--- biliob_analyzer/video_analyzer.py
+++ biliob_analyzer/video_analyzer.py
@@ -41,15 +41,17 @@ class VideoAnalyzer(object):
                     rate = (c_view-pre_view)
                     pre_view = c_view
                     pre_date = c_date
-
+                    # 三天内播放增长小于3000则被认定为低质量
                     if live_time == 3 and c_view < 3000:
                         delete = True
                         focus = False
                         break
+                    # 大于三天后每日播放增长小于100则停止追踪
                     elif live_time > 3 and rate < 100:
                         focus = False
                         delete = False
                         break
+                    # 除此之外的情况则持续追踪
                     else:
                         focus = True
                         delete = False
