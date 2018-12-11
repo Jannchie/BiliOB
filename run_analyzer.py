6df463397f316d944467cd00bc937c05a988bca3
Jannchie <jannchie@gmail.com>
2018-12-11 15:24:44 +0800

    feature: auto caculate author fans change rant

--- run_analyzer.py
+++ run_analyzer.py
@@ -1,5 +1,6 @@
 from biliob_analyzer.author_analyzer import AuthorAnalyzer
 from biliob_analyzer.video_analyzer import VideoAnalyzer
+import biliob_analyzer.author_rate_caculate
 
 author_analyzer = AuthorAnalyzer()
 video_analyzer = VideoAnalyzer()
