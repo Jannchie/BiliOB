c04f5ccd36eb1d875389177e31a6c5749b25fbad
Jannchie <jannchie@gmail.com>
2019-01-20 21:00:47 +0800

    bangumi & donghua

--- biliob_spider/spiders/bangumi.py
+++ biliob_spider/spiders/bangumi.py
@@ -1,6 +1,6 @@
 #coding=utf-8
 import scrapy
-from scrapy.mail import MailSender
+from mail import mailer
 from scrapy.http import Request
 from biliob_spider.items import BangumiItem
 import time
@@ -19,21 +19,31 @@ class BangumiSpider(scrapy.spiders.Spider):
     }
 
     def parse(self, response):
-        j = json.loads(response.xpath("//script[3]/text()").extract()[0][len('window.__INITIAL_STATE__='):].split(';')[0])
-        for each in j['rankList']:
-            item = BangumiItem()
-            item['title'] = each['title']
-            item['cover'] = each['cover']
-            item['square_cover'] = each['square_cover']
-            item['is_finish'] = each['is_finish']
-            item['is_started'] = each['is_started']
-            item['newest_ep_index'] = each['newest_ep_index']
-            item['data'] = {
-                'danmaku': each['dm_count'],
-                'watch': each['fav'],
-                'play': each['play'],
-                'pts': each['pts'],
-                'review': each['video_review'],
-                'datetime': datetime.datetime.now()
-            }
-            yield item
+        try:
+            j = json.loads(response.xpath("//script[3]/text()").extract()[0][len('window.__INITIAL_STATE__='):].split(';')[0])
+            for each in j['rankList']:
+                item = BangumiItem()
+                item['title'] = each['title']
+                item['cover'] = each['cover']
+                # item['square_cover'] = each['square_cover']
+                # item['is_finish'] = each['is_finish']
+                # item['is_started'] = each['is_started']
+                item['newest_ep_index'] = each['new_ep']['index_show']
+                item['data'] = {
+                    'danmaku': each['stat']['danmaku'],
+                    'watch': each['stat']['follow'],
+                    'play': each['stat']['view'],
+                    'pts': each['pts'],
+                    'review': each['video_review'],
+                    'datetime': datetime.datetime.now()
+                }
+                yield item
+        except Exception as error:
+            # 出现错误时打印错误日志
+            mailer.send(
+                to=["604264970@qq.com"],
+                subject="BiliobSpiderError",
+                body="{}\n{}".format(response.url, error),
+            )
+
+
