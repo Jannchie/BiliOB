62465ed69ba7ec7c53e9b1b555757e37db939c50
Jannchie <jannchie@gmail.com>
2019-01-20 21:00:47 +0800

    bangumi & donghua

--- biliob_spider/pipelines.py
+++ biliob_spider/pipelines.py
@@ -6,8 +6,10 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from pymongo import MongoClient
 from db import settings
+from db import mysql_connect
 import datetime
 import logging
+
 
 class VideoPipeline(object):
     def __init__(self):
@@ -25,13 +27,13 @@ class VideoPipeline(object):
                 'aid': int(item['aid'])
             }, {
                 '$set': {
-                    'cView':item['current_view'],
-                    'cFavorite':item['current_favorite'],
-                    'cDanmaku':item['current_danmaku'] ,
-                    'cCoin':item['current_coin'],
-                    'cShare':item['current_share'] ,
-                    'cLike':item['current_like'],
-                    'cDatetime':item['current_datetime'],
+                    'cView': item['current_view'],
+                    'cFavorite': item['current_favorite'],
+                    'cDanmaku': item['current_danmaku'],
+                    'cCoin': item['current_coin'],
+                    'cShare': item['current_share'],
+                    'cLike': item['current_like'],
+                    'cDatetime': item['current_datetime'],
                     'author': item['author'],
                     'subChannel': item['subChannel'],
                     'channel': item['channel'],
@@ -43,8 +45,42 @@ class VideoPipeline(object):
                 },
                 '$push': {
                     'data': {
-                        '$each':[item['data']],
-                        '$position':0
+                        '$each': [item['data']],
+                        '$position': 0
+                    }
+                }
+            }, True)
+            return item
+        except Exception as error:
+            # 出现错误时打印错误日志
+            logging.error(error)
+
+class VideoPipelineFromKan(object):
+    def __init__(self):
+        # 链接mongoDB
+        self.client = MongoClient(settings['MINGO_HOST'], 27017)
+        # 数据库登录需要帐号密码
+        self.client.admin.authenticate(settings['MINGO_USER'],
+                                       settings['MONGO_PSW'])
+        self.db = self.client['biliob']  # 获得数据库的句柄
+        self.coll = self.db['video']  # 获得collection的句柄
+
+    def process_item(self, item, spider):
+        try:
+            self.coll.update_one({
+                'aid': int(item['aid'])
+            }, {
+                '$set': {
+                    'author': item['author'],
+                    'mid': item['mid'],
+                    'pic': item['pic'],
+                    'title': item['title'],
+                    'datetime': item['datetime']
+                },
+                '$push': {
+                    'data': {
+                        '$each': [item['data']],
+                        '$position': 0
                     }
                 }
             }, True)
@@ -71,12 +107,12 @@ class BangumiPipeLine(object):
                 '$set': {
                     'title': item['title'],
                     'cover': item['cover'],
-                    'isFinish': item['is_finish'],
-                    'isStarted': item['is_started'],
+                    # 'isFinish': item['is_finish'],
+                    # 'isStarted': item['is_started'],
                     'newest': item['newest_ep_index'],
                     'currentPts': item['data']['pts'],
                     'currentPlay': item['data']['play'],
-                    'squareCover': item['square_cover'],
+                    # 'squareCover': item['square_cover'],
                     'currentWatch': item['data']['watch'],
                     'currentReview': item['data']['review'],
                     'currentDanmaku': item['data']['danmaku']
@@ -89,6 +125,7 @@ class BangumiPipeLine(object):
         except Exception as error:
             # 出现错误时打印错误日志
             logging.error(error)
+
 
 class DonghuaPipeLine(object):
     def __init__(self):
@@ -108,12 +145,12 @@ class DonghuaPipeLine(object):
                 '$set': {
                     'title': item['title'],
                     'cover': item['cover'],
-                    'isFinish': item['is_finish'],
-                    'isStarted': item['is_started'],
+                    # 'isFinish': item['is_finish'],
+                    # 'isStarted': item['is_started'],
                     'newest': item['newest_ep_index'],
                     'currentPts': item['data']['pts'],
                     'currentPlay': item['data']['play'],
-                    'squareCover': item['square_cover'],
+                    # 'squareCover': item['square_cover'],
                     'currentWatch': item['data']['watch'],
                     'currentReview': item['data']['review'],
                     'currentDanmaku': item['data']['danmaku']
@@ -126,6 +163,8 @@ class DonghuaPipeLine(object):
         except Exception as error:
             # 出现错误时打印错误日志
             logging.error(error)
+
+
 class SiteInfoPipeline(object):
     def __init__(self):
         # 链接mongoDB
@@ -139,16 +178,17 @@ class SiteInfoPipeline(object):
     def process_item(self, item, spider):
         try:
             self.coll.insert_one({
-                    'region_count': item['region_count'],
-                    'all_count': item['all_count'],
-                    'web_online': item['web_online'],
-                    'play_online': item['play_online'],
-                    'datetime':datetime.datetime.now()
+                'region_count': item['region_count'],
+                'all_count': item['all_count'],
+                'web_online': item['web_online'],
+                'play_online': item['play_online'],
+                'datetime': datetime.datetime.now()
             })
             return item
         except Exception as error:
             # 出现错误时打印错误日志
             logging.error(error)
+
 
 class AuthorPipeline(object):
     def __init__(self):
@@ -166,23 +206,23 @@ class AuthorPipeline(object):
                 'mid': item['mid']
             }, {
                 '$set': {
-                    'focus':True,
+                    'focus': True,
                     'sex': item['sex'],
                     'name': item['name'],
                     'face': item['face'],
                     'level': item['level'],
-                    'cFans':item['c_fans'],
+                    'cFans': item['c_fans'],
                     'official': item['official'],
-                    'cArchive':item['c_archive'] ,
-                    'cArticle':item['c_article'] ,
-                    'cAttention':item['c_attention'] ,
-                    'cArchive_view':item['c_archive_view'],
-                    'cArticle_view':item['c_article_view'], 
+                    'cArchive': item['c_archive'],
+                    'cArticle': item['c_article'],
+                    'cAttention': item['c_attention'],
+                    'cArchive_view': item['c_archive_view'],
+                    'cArticle_view': item['c_article_view'],
                 },
                 '$push': {
                     'data': {
-                        '$each':[item['data']],
-                        '$position':0
+                        '$each': [item['data']],
+                        '$position': 0
                     }
                 }
             }, True)
@@ -190,7 +230,8 @@ class AuthorPipeline(object):
         except Exception as error:
             # 出现错误时打印错误日志
             logging.error(error)
-        
+
+
 class OnlinePipeline(object):
     def __init__(self):
         # 链接mongoDB
@@ -203,7 +244,7 @@ class OnlinePipeline(object):
 
     def process_item(self, item, spider):
         try:
-            
+
             self.coll.update_one({
                 'title': item['title']
             }, {
@@ -235,7 +276,7 @@ class TagPipeLine(object):
 
     def process_item(self, item, spider):
         try:
-            
+
             self.coll.update_one({
                 'tag_id': item['tag_id']
             }, {
@@ -253,6 +294,8 @@ class TagPipeLine(object):
         except Exception as error:
             # 出现错误时打印错误日志
             logging.error(error)
+
+
 class VideoAddPipeline(object):
     def __init__(self):
         # 链接mongoDB
@@ -279,6 +322,7 @@ class VideoAddPipeline(object):
             # 出现错误时打印错误日志
             logging.error(error)
 
+
 class AuthorChannelPipeline(object):
     def __init__(self):
         # 链接mongoDB
@@ -303,6 +347,7 @@ class AuthorChannelPipeline(object):
             # 出现错误时打印错误日志
             logging.error(error)
 
+
 class BiliMonthlyRankPipeline(object):
     def __init__(self):
         # 链接mongoDB
@@ -322,13 +367,13 @@ class BiliMonthlyRankPipeline(object):
                     'pts': item['pts'],
                     'datetime': datetime.datetime.now()
                 },
-                '$set':{
+                '$set': {
                     'title': item['title'],
                     'author': item['author'],
                     'aid': item['aid'],
                     'mid': item['mid'],
                     'channel': item['channel'],
-                    'currentPts':item['pts']
+                    'currentPts': item['pts']
                 }
             }, True)
             return item
