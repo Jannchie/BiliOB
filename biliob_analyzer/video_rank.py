a8177b7ca6fe859200b59d09dbb5125b9183df2c
Jannchie <jannchie@gmail.com>
2019-02-14 14:35:24 +0800

    feature: video rank algorithm

--- biliob_analyzer/video_rank.py
+++ biliob_analyzer/video_rank.py
@@ -15,25 +15,49 @@ i = 1
 
 keys = ['cView', 'cLike', 'cDanmaku', 'cFavorite', 'cCoin', 'cShare']
 for each_key in keys:
-    rank_key = each_key[1:] + 'Rank'
     logger.info("开始计算视频{}排名".format(each_key))
-    videos = coll.find({}, {'aid': 1, 'rank': 1, each_key: 1}).batch_size(
-        20).sort(each_key, DESCENDING)
+    i = 1
+    videos = coll.find({each_key: {'$exists': 1}}, {'aid': 1, 'rank': 1, each_key: 1}).batch_size(
+        300).sort(each_key, DESCENDING)
+    if each_key == 'cView':
+        each_rank = 'cViewRank'
+        each_d_rank = 'dViewRank'
+
+    each_rank = each_key + 'Rank'
+    each_d_rank = 'd' + each_key[1:] + 'Rank'
+
     for each_video in videos:
         # 如果没有data 直接下一个
         if each_key in each_video:
             if 'rank' in each_video:
                 rank = each_video['rank']
-                rank[rank_key] = i
+                if each_rank in each_video['rank']:
+                    rank[each_d_rank] = each_video['rank'][each_rank] - i
+                else:
+                    rank[each_d_rank] = -1
+                rank[each_rank] = i
             else:
                 rank = {
-                    rank_key: i
+                    each_rank: i,
+                    each_d_rank: -1
                 }
-            i += 1
-            coll.update_one({'aid': each_video['aid']}, {
-                '$set': {
-                    'rank': rank,
+        if each_video[each_key] == 0:
+            if 'rank' in each_video:
+                rank = each_video['rank']
+                rank[each_d_rank] = 0
+                rank[each_rank] = -1
+            else:
+                rank = {
+                    each_rank: -1,
+                    each_d_rank: 0
                 }
-            })
+        if each_key == keys[-1]:
+            rank['updateTime'] = datetime.datetime.now()
+        coll.update_one({'aid': each_video['aid']}, {
+            '$set': {
+                'rank': rank,
+            }
+        })
+        i += 1
 
     logger.info("完成计算视频数据排名")
