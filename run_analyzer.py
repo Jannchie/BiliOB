5059391d2222c296f1a55335fb4bc58ed66bf507
Jannchie <jannchie@gmail.com>
2019-02-15 16:48:26 +0800

    feature: video rank

--- run_analyzer.py
+++ run_analyzer.py
@@ -3,6 +3,7 @@ from biliob_analyzer.video_analyzer import VideoAnalyzer
 import biliob_analyzer.author_rate_caculate
 import biliob_analyzer.author_fans_watcher
 import biliob_analyzer.author_rank
+import biliob_analyzer.video_rank
 
 author_analyzer = AuthorAnalyzer()
 video_analyzer = VideoAnalyzer()
