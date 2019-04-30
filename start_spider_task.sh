ea83b3f7aba015fec00177714315ef023842fafb
Jannchie <jannchie@gmail.com>
2019-04-30 20:48:26 +0800

    update: start spider task shell script

--- start_spider_task.sh
+++ start_spider_task.sh
@@ -1,17 +1,13 @@
-ps -ef | grep authorRedis | grep -v grep | cut -c 9-15 | xargs kill -9
-ps -ef | grep videoRedis | grep -v grep | cut -c 9-15 | xargs kill -9
-ps -ef | grep bangumiAndDonghua | grep -v grep | cut -c 9-15 | xargs kill -9
-ps -ef | grep authorAutoAdd | grep -v grep | cut -c 9-15 | xargs kill -9
-ps -ef | grep videoAutoAdd | grep -v grep | cut -c 9-15 | xargs kill -9
-ps -ef | grep site | grep -v grep | cut -c 9-15 | xargs kill -9
-ps -ef | grep DanmakuAggregate | grep -v grep | cut -c 9-15 | xargs kill -9
+#!/bin/bash
+spiders=(authorRedis videoRedis bangumiAndDonghua authorAutoAdd videoAutoAdd site online)
 
-nohup scrapy crawl authorRedis 1>log.log 2>&1 &
-nohup scrapy crawl videoRedis 1>log.log 2>&1 &
-nohup scrapy crawl bangumiAndDonghua 1>log.log 2>&1 &
-nohup scrapy crawl authorAutoAdd 1>log.log 2>&1 &
-nohup scrapy crawl videoAutoAdd 1>log.log 2>&1 &
-nohup scrapy crawl site 1>log.log 2>&1 &
+for var in ${spiders[@]} 
+do 
+    ps -ef | grep $var | grep -v grep | cut -c 9-15 | xargs kill -9 
+    nohup scrapy crawl $var 1>log.log 2>&1 &
+done 
 
+
+ps -ef | grep DanmakuAggregate | grep -v grep | cut -c 9-15 | xargs kill -9 
 cd danmaku_spider/ && nohup scrapy crawl DanmakuAggregate 1>log.log 2>&1 &
 ps -ef |grep py
