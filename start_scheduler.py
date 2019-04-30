a16e6095ee13b962a8961e8c506cededbcfec4d1
Jannchie <jannchie@gmail.com>
2019-04-30 20:52:10 +0800

    update: start scheduler script

--- start_scheduler.py
+++ start_scheduler.py
@@ -1,16 +1,2 @@
-from biliob_scheduler.scheduler import auto_crawl_bangumi
-from biliob_scheduler.scheduler import auto_add_video
-from biliob_scheduler.scheduler import auto_add_author
-from biliob_scheduler.scheduler import crawlOnlineTopListData
-from biliob_scheduler.scheduler import update_author
-from biliob_scheduler.scheduler import update_unfocus_video
-from biliob_scheduler.scheduler import update_video
-from biliob_scheduler.scheduler import gen_online
-auto_crawl_bangumi()
-auto_add_video()
-auto_add_author()
-crawlOnlineTopListData()
-update_author()
-update_unfocus_video()
-update_video()
-gen_online()
+from biliob_scheduler.scheduler import auto_crawl_task
+auto_crawl_task()
