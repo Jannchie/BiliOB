aaa46f465d96028a7472dde2a8c5d77d6ef990d4
Jannchie <jannchie@gmail.com>
2019-06-11 11:07:06 +0800

    update: restart system

--- biliob_requests/author_update_currentFans.py
+++ biliob_requests/author_update_currentFans.py
@@ -1,3 +1,4 @@
+from requests.adapters import HTTPAdapter
 import time
 import datetime
 from db import settings
@@ -11,24 +12,30 @@ client.admin.authenticate(settings['MINGO_USER'],
 db = client['biliob']  # 获得数据库的句柄
 author = db['author']  # 获得collection的句柄
 realtime_fans = db['realtime_fans']
-
 URL = 'https://api.bilibili.com/x/web-interface/card?mid={}'
-while True:
-    docs = author.find({}, {'mid': 1}).sort(
-        'cFans', direction=DESCENDING).limit(2)
-    mids = map(lambda x: x['mid'], docs)
-    date = datetime.datetime.now()
-    for mid in mids:
-        try:
-            j = requests.get(URL.format(mid)).json()
-            pass
-            fans = j['data']['card']['fans']
-            if fans == 0:
-                continue
-            author.update_one({'mid': mid}, {'$set': {'cFans': fans}})
-            realtime_fans.insert_one(
-                {'mid': mid, 'fans': fans, 'datetime': date})
-        except Exception as e:
-            print(e)
-            pass
-    time.sleep(5)
+
+
+def run():
+    while True:
+        docs = author.find({}, {'mid': 1}).sort(
+            'cFans', direction=DESCENDING).limit(2)
+        mids = map(lambda x: x['mid'], docs)
+        date = datetime.datetime.now()
+        for mid in mids:
+            try:
+                j = requests.get(URL.format(mid)).json()
+                pass
+                fans = j['data']['card']['fans']
+                if fans == 0:
+                    continue
+                author.update_one({'mid': mid}, {'$set': {'cFans': fans}})
+                realtime_fans.insert_one(
+                    {'mid': mid, 'fans': fans, 'datetime': date})
+            except Exception as e:
+                print(e)
+                run()
+                pass
+        time.sleep(5)
+
+
+run()
