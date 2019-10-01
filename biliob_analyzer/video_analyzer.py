dc6880ab8457e469677575d1961899bd966bc1b0
jannchie <jannchie@gmail.com>
2019-10-01 10:24:43 +0900

    update: video analyzer

--- biliob_analyzer/video_analyzer.py
+++ biliob_analyzer/video_analyzer.py
@@ -28,7 +28,7 @@ class VideoAnalyzer(object):
         count_delete = 0
         count_unfocus = 0
         count_focus = 0
-        for each_doc in self.coll.find({'focus': True, 'cView': {'lt': 100000}}):
+        for each_doc in self.coll.find({'focus': True}):
             live_time = 0
             delete = False
             focus = True
@@ -49,13 +49,13 @@ class VideoAnalyzer(object):
                     rate = (c_view-pre_view)
                     pre_view = c_view
                     pre_date = c_date
-                    # # 三天内播放增长小于3000则被认定为低质量
-                    # if live_time == 3 and c_view < 3000:
-                    #     delete = True
-                    #     focus = False
-                    #     break
-                    # 大于7天后每日播放增长小于100则停止追踪
-                    if live_time > 7 and rate < 100:
+                    # 三天内播放增长小于3000则被认定为低质量
+                    if live_time == 3 and c_view < 3000:
+                        delete = False
+                        focus = False
+                        break
+                    # 大于7天后每日播放增长小于10000则停止追踪
+                    if live_time > 7 and rate < 10000:
                         focus = False
                         delete = False
                         break
