bc51249a216b31e759f7baf92b79f2203a19508f
jannchie <jannchie@gmail.com>
2019-10-18 23:54:19 +0900

    fix

--- biliob_requests/check_mogo.py
+++ biliob_requests/check_mogo.py
@@ -8,7 +8,10 @@ def check():
     while True:
         sleep(10)
         try:
-            db['blacklist'].find_one()
+            if len(db.current_op()['inprog']) >= 30:
+                print('{} 链接失败'.format(datetime.datetime.now()))
+                os.system('systemctl restart mongod')
+                check()
         except Exception:
             print('{} 链接失败'.format(datetime.datetime.now()))
             os.system('systemctl restart mongod')
