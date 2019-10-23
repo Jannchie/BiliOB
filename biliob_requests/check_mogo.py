c3e875d55a8dbaab777a453440f231cc94059f22
jannchie <jannchie@gmail.com>
2019-10-23 11:16:16 +0900

    update: check

--- biliob_requests/check_mogo.py
+++ biliob_requests/check_mogo.py
@@ -1,21 +1,27 @@
-from db import db
+from db import client
 from time import sleep
 import os
 import datetime
 
 
 def check():
+    db = client['admin']
     while True:
         sleep(5)
         try:
-            if len(db.current_op()['inprog']) >= 100:
+            l = db.command("serverStatus")[
+                'globalLock']['currentQueue']['total']
+            if l >= 1:
                 print('{} 链接失败'.format(datetime.datetime.now()))
                 os.system('systemctl restart mongod')
-                check()
         except Exception:
             print('{} 链接失败'.format(datetime.datetime.now()))
             os.system('systemctl restart mongod')
             check()
 
+
+# db.currentOp().inprog.forEach(function(cop){
+# db.killOp(cop.opid)
+# })
 
 check()
